c11a2b7c55c270a23aed74f06d33b9a8eabdc0c7
Dede Firdaus <dedefirdausxd@gmail.com>
2026-03-22 00:11:05 +0700

    feat(praktikum-5): add hasil praktikum 5

---
+++ praktikum/praktikum-5/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo "Hello World <br>"; ?>
+
+    <?php
+    $nama = "Budi";
+    $umur = 20;
+
+    // Menampilkan nilai variabel
+    echo "Nama: " . $nama . "<br>";
+    echo "Umur: " . $umur . " tahun<br>";
+    ?>
+</body>
+
+</html>
